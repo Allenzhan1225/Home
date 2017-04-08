af0d5b21c626fe8f2591b91369377a8cc87765a6
Allenzhan1225 <1042748558@qq.com>
2017-04-08 16:06:28 +0800

    添加了IJKFremWork

--- Home/classess/首页/View/YMLiveViewViewController.swift
+++ Home/classess/首页/View/YMLiveViewViewController.swift
@@ -8,17 +8,25 @@
 //
 
 import UIKit
-
+import IJKMediaFramework
 
 
 class YMLiveViewViewController: UIViewController {
     
     var webView:UIWebView?
-     var anchorModel : HomeModel!
+    var anchorModel : HomeModel!
+    var ijkLivePlay : IJKFFMoviePlayerController!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         setupUI()
+        
+        
+        
+        //你好
         
         // Do any additional setup after loading the view.
     }
@@ -47,15 +55,47 @@ class YMLiveViewViewController: UIViewController {
 extension YMLiveViewViewController{
     fileprivate func setupUI(){
         
-        webView = UIWebView(frame: self.view.bounds)
-        view.addSubview(webView!)
+//        webView = UIWebView(frame: self.view.bounds)
+//        view.addSubview(webView!)
+//        
+//  
+//        guard let requestUrl =  URL(string: self.anchorModel.share_addr ?? "")  else { return  }
+//        
+//        print(requestUrl)
+//        
+//        webView?.loadRequest(URLRequest(url: requestUrl))
         
-  
-        guard let requestUrl =  URL(string: self.anchorModel.share_addr ?? "")  else { return  }
+        view.backgroundColor = UIColor.clear
+        let requsetUrl = URL(string: self.anchorModel.stream_addr ?? "")
+        print(requsetUrl!)
+        //00 设置不报告日志
+//        IJKFFMoviePlayerController.setLogReport(false)
+//        IJKFFMoviePlayerController.setLogLevel(k_IJK_LOG_INFO)
         
-        print(requestUrl)
+        //01 默认选项配置
+        let  options  =  IJKFFOptions.byDefault()
+
+        //0. 创建播放控制器
+        ijkLivePlay = IJKFFMoviePlayerController(contentURL: requsetUrl, with: options)
         
-        webView?.loadRequest(URLRequest(url: requestUrl))
+        //1.设置播放器布满整个屏幕
+        ijkLivePlay.view.frame = view.bounds
+        
+        //2.设置适配横竖屏幕（设置四边固定，长度灵活）
+        ijkLivePlay.view.autoresizingMask = [.flexibleWidth,.flexibleHeight]
+        
+        //3. 设置播放器视图的缩放模式
+        ijkLivePlay.scalingMode = .aspectFill
+
+        //4. 设置自动播放
+        ijkLivePlay.shouldAutoplay = true
+
+        //5. 自动更新子视图的大小
+        view.autoresizesSubviews = true
+
+        view.addSubview(self.ijkLivePlay.view)
+    
+        
     }
 }
 
